8ed87a4d56c4ee21d4ca97b8629405d19ba14e0d
Talisson Augusto <prudencioiluminado@gmail.com>
2025-05-09 23:44:59 -0300

    Configuração do Banco de Dados e da Tabela Veiculo

---
+++ mf-dev-backend-2023/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatório inserir o nome do veículo")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório inserir a placa do veículo")]
+        public string Placa { get; set; }
+        [Required(ErrorMessage = "Obrigatório inserir o Ano de Fabricação")]
+        public int AnoFabricacao { get; set; }
+        [Required(ErrorMessage = "Obrigatório inserir o Ano do Modelo")]
+        public int AnoModelo { get; set; }
+    }
+}
